f9ba488173384efab2121155e7500ccf12e7fbf8
Margaret Lazarovits <margaretlazarovits@Margarets-MacBook-Air-2.local>
2019-06-24 14:53:29 -0500

    1L mass plot

--- macros/Plot_1D_stack.C
+++ macros/Plot_1D_stack.C
@@ -148,10 +148,10 @@ void Plot_1D_stack(){
   int Nsample = g_Samples.size();
   cout << "Nsample: " << Nsample << endl;
 
-  g_PlotTitle = "Single Lepton MET";
+  g_PlotTitle = "Single Lepton Mass, Cat. 1";
   g_Lumi = 100;
 
-  g_Xname = "MET";
+  g_Xname = "1L Mass";
   g_Xmin = 0;
 
 
@@ -160,12 +160,12 @@ void Plot_1D_stack(){
   // units_per_bin = 0.01;
 
   //M_lep
-  // g_Xmax = 1.5;
-  // units_per_bin = 0.01;
+  g_Xmax = 1.5;
+  units_per_bin = 0.01;
 
   //MET
-  g_Xmax = 1500.;
-  units_per_bin = 50.;
+  // g_Xmax = 1500.;
+  // units_per_bin = 50.;
 
   g_NX = (int)(g_Xmax - g_Xmin)/units_per_bin;
 
@@ -224,7 +224,7 @@ void Plot_1D_stack(){
 	// if(base->PDGID_lep->at(0)+base->PDGID_lep->at(1) != 0)
 	//   continue;
 	
-	hist[s]->Fill(base->MET, base->weight*g_Lumi*double(SKIP));
+	hist[s]->Fill(base->M_lep->at(0), base->weight*g_Lumi*double(SKIP));
       }
 
       delete base;
