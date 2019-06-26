256470022db9364600a5aa253243d8708aa9d2ea
Margaret Lazarovits <margaretlazarovits@Margarets-MacBook-Air-2.local>
2019-06-26 10:00:46 -0500

    risr ptisr cat 1 background 17

--- macros/Plot_2D.C
+++ macros/Plot_2D.C
@@ -43,41 +43,41 @@ void Plot_2D(){
   int BKG_SKIP = 10; //takes 1 in every BKG_SKIP events
 
 
-//   SampleSet ttX;
-//   ttX.SetBkg(true);
-//   ttX.SetTitle("t#bar{t} + X");
-//   ttX.SetColor(kAzure+1);
-//   ttX.AddFile(StopNtuplePath+"All_Bkg_2017/TTJets_TuneCP5_13TeV-amcatnloFXFX-pythia8_TuneCP5.root");
-//   // ttX.AddFile(StopNtuplePath+"All_Bkg_2017/TTJets_DiLept_TuneCP5_13TeV-madgraphMLM-pythia8_Fall17.root");
-//   // ttX.AddFile(StopNtuplePath+"All_Bkg_2017/TTJets_SingleLeptFromT_TuneCP5_13TeV-madgraphMLM-pythia8_Fall17.root");
-//   // ttX.AddFile(StopNtuplePath+"All_Bkg_2017/TTJets_SingleLeptFromTbar_TuneCP5_13TeV-madgraphMLM-pythia8_Fall17.root");
-//   // ttX.AddFile(StopNtuplePath+"All_Bkg_2017/ttWJets_TuneCP5_13TeV_madgraphMLM_pythia8_Fall17.root");
-//   // ttX.AddFile(StopNtuplePath+"All_Bkg_2017/ttZJets_TuneCP5_13TeV_madgraphMLM_pythia8_Fall17.root");
-//   // ttX.AddFile(StopNtuplePath+"All_Bkg_2017/TTGJets_TuneCP5_13TeV-amcatnloFXFX-madspin-pythia8_Fall17.root");
-//   ttX.SetSkip(10);
-//   g_Samples.push_back(&ttX);
+  SampleSet ttX;
+  ttX.SetBkg(true);
+  ttX.SetTitle("t#bar{t} + X");
+  ttX.SetColor(kAzure+1);
+  ttX.AddFile(StopNtuplePath+"All_Bkg_2017/TTJets_TuneCP5_13TeV-amcatnloFXFX-pythia8_TuneCP5.root");
+  // ttX.AddFile(StopNtuplePath+"All_Bkg_2017/TTJets_DiLept_TuneCP5_13TeV-madgraphMLM-pythia8_Fall17.root");
+  // ttX.AddFile(StopNtuplePath+"All_Bkg_2017/TTJets_SingleLeptFromT_TuneCP5_13TeV-madgraphMLM-pythia8_Fall17.root");
+  // ttX.AddFile(StopNtuplePath+"All_Bkg_2017/TTJets_SingleLeptFromTbar_TuneCP5_13TeV-madgraphMLM-pythia8_Fall17.root");
+  // ttX.AddFile(StopNtuplePath+"All_Bkg_2017/ttWJets_TuneCP5_13TeV_madgraphMLM_pythia8_Fall17.root");
+  // ttX.AddFile(StopNtuplePath+"All_Bkg_2017/ttZJets_TuneCP5_13TeV_madgraphMLM_pythia8_Fall17.root");
+  // ttX.AddFile(StopNtuplePath+"All_Bkg_2017/TTGJets_TuneCP5_13TeV-amcatnloFXFX-madspin-pythia8_Fall17.root");
+  ttX.SetSkip(10);
+  g_Samples.push_back(&ttX);
 
-// SampleSet DYjets;
-//   DYjets.SetBkg(true);
-//   DYjets.SetTitle("DY + jets");
-//   DYjets.SetColor(kGreen-7);
-//   DYjets.AddFile(StopNtuplePath+"All_Bkg_2017/DYJetsToLL_M-5to50_TuneCP5_13TeV-madgraphMLM-pythia8_Fall17.root");
-//   DYjets.AddFile(StopNtuplePath+"All_Bkg_2017/DYJetsToLL_M-50_TuneCP5_13TeV-amcatnloFXFX-pythia8_Fall17.root");
-//   DYjets.SetSkip(BKG_SKIP);
-//   g_Samples.push_back(&DYjets);
+SampleSet DYjets;
+  DYjets.SetBkg(true);
+  DYjets.SetTitle("DY + jets");
+  DYjets.SetColor(kGreen-7);
+  DYjets.AddFile(StopNtuplePath+"All_Bkg_2017/DYJetsToLL_M-5to50_TuneCP5_13TeV-madgraphMLM-pythia8_Fall17.root");
+  DYjets.AddFile(StopNtuplePath+"All_Bkg_2017/DYJetsToLL_M-50_TuneCP5_13TeV-amcatnloFXFX-pythia8_Fall17.root");
+  DYjets.SetSkip(BKG_SKIP);
+  g_Samples.push_back(&DYjets);
 
-//   SampleSet Wjets;
-//   Wjets.SetBkg(true);
-//   Wjets.SetTitle("W + jets");
-//   Wjets.SetColor(kRed-7);
-//   Wjets.AddFile(StopNtuplePath+"All_Bkg_2017/WJetsToLNu_HT-100To200_TuneCP5_13TeV-madgraphMLM-pythia8_Fall17.root");
-//   Wjets.AddFile(StopNtuplePath+"All_Bkg_2017/WJetsToLNu_HT-1200To2500_TuneCP5_13TeV-madgraphMLM-pythia8_Fall17.root");
-//   Wjets.AddFile(StopNtuplePath+"All_Bkg_2017/WJetsToLNu_HT-200To400_TuneCP5_13TeV-madgraphMLM-pythia8_Fall17.root");
-//   Wjets.AddFile(StopNtuplePath+"All_Bkg_2017/WJetsToLNu_HT-400To600_TuneCP5_13TeV-madgraphMLM-pythia8_Fall17.root");
-//   Wjets.AddFile(StopNtuplePath+"All_Bkg_2017/WJetsToLNu_HT-600To800_TuneCP5_13TeV-madgraphMLM-pythia8_Fall17.root");
-//   Wjets.AddFile(StopNtuplePath+"All_Bkg_2017/WJetsToLNu_HT-800To1200_TuneCP5_13TeV-madgraphMLM-pythia8_Fall17.root");
-//   Wjets.SetSkip(BKG_SKIP);
-//   g_Samples.push_back(&Wjets);
+  SampleSet Wjets;
+  Wjets.SetBkg(true);
+  Wjets.SetTitle("W + jets");
+  Wjets.SetColor(kRed-7);
+  Wjets.AddFile(StopNtuplePath+"All_Bkg_2017/WJetsToLNu_HT-100To200_TuneCP5_13TeV-madgraphMLM-pythia8_Fall17.root");
+  Wjets.AddFile(StopNtuplePath+"All_Bkg_2017/WJetsToLNu_HT-1200To2500_TuneCP5_13TeV-madgraphMLM-pythia8_Fall17.root");
+  Wjets.AddFile(StopNtuplePath+"All_Bkg_2017/WJetsToLNu_HT-200To400_TuneCP5_13TeV-madgraphMLM-pythia8_Fall17.root");
+  Wjets.AddFile(StopNtuplePath+"All_Bkg_2017/WJetsToLNu_HT-400To600_TuneCP5_13TeV-madgraphMLM-pythia8_Fall17.root");
+  Wjets.AddFile(StopNtuplePath+"All_Bkg_2017/WJetsToLNu_HT-600To800_TuneCP5_13TeV-madgraphMLM-pythia8_Fall17.root");
+  Wjets.AddFile(StopNtuplePath+"All_Bkg_2017/WJetsToLNu_HT-800To1200_TuneCP5_13TeV-madgraphMLM-pythia8_Fall17.root");
+  Wjets.SetSkip(BKG_SKIP);
+  g_Samples.push_back(&Wjets);
 
   // SampleSet ST;
   // ST.SetBkg(true);
@@ -127,19 +127,19 @@ void Plot_2D(){
   // g_Samples.push_back(&SIG2);
 
 
-  SampleSet SIG3;
-  SIG3.SetBkg(false);
-  SIG3.SetTitle("m_{#chi^{#pm}_{1}/#chi^{0}_{2}} = 200, m_{#chi^{0}_{1}} = 1");
-  SIG3.SetTreeName("SMS_200_1");
-  SIG3.SetColor(kViolet-7);
-  SIG3.AddFile(StopNtuplePath+"All_Sig/SMS-TChiWZ_ZToLL_mZMin-0p1_TuneCP2_13TeV-madgraphMLM-pythia8_Fall17.root");
-  SIG3.SetSkip(1);
-  g_Samples.push_back(&SIG3);
+  // SampleSet SIG3;
+  // SIG3.SetBkg(false);
+  // SIG3.SetTitle("m_{#chi^{#pm}_{1}/#chi^{0}_{2}} = 200, m_{#chi^{0}_{1}} = 1");
+  // SIG3.SetTreeName("SMS_200_1");
+  // SIG3.SetColor(kViolet-7);
+  // SIG3.AddFile(StopNtuplePath+"All_Sig/SMS-TChiWZ_ZToLL_mZMin-0p1_TuneCP2_13TeV-madgraphMLM-pythia8_Fall17.root");
+  // SIG3.SetSkip(1);
+  // g_Samples.push_back(&SIG3);
 
   int Nsample = g_Samples.size();
   
   //string g_Label = "No selection";
-  string g_Label = "RISR vs. pT_ISR, Cat. 3";
+  string g_Label = "RISR vs. pT_ISR, Cat. 1";
 
 
   g_Xname = "R_ISR";
@@ -193,7 +193,7 @@ void Plot_2D(){
   if(base->Nlep != 1)
     continue;
       
-	hist->Fill(base->RISR->at(2), base->PTISR->at(2) , base->weight*double(SKIP));
+	hist->Fill(base->RISR->at(0), base->PTISR->at(0) , base->weight*double(SKIP));
       }
 
       delete base;
@@ -253,7 +253,7 @@ void Plot_2D(){
   // l.DrawLatex(0.74,0.04,g_Label.c_str());
 
 
-  TString file_name = "output/"+g_Label+"signal199.root";
+  TString file_name = "output/"+g_Label+"bkg17.root";
   TFile* file = new TFile(file_name,"RECREATE");
   file->cd();
   can->Write();
