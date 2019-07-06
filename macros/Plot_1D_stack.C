ad153900a296dad898aac6cd194cba4432c1b89d
Margaret Lazarovits <margaretlazarovits@Margarets-MacBook-Air-2.local>
2019-07-06 18:18:42 -0500

    eta for muon

--- macros/Plot_1D_stack.C
+++ macros/Plot_1D_stack.C
@@ -148,10 +148,10 @@ void Plot_1D_stack(){
   int Nsample = g_Samples.size();
   cout << "Nsample: " << Nsample << endl;
 
-  g_PlotTitle = "#eta_{e}";
+  g_PlotTitle = "#eta_{#mu}";
   g_Lumi = 100;
 
-  g_Xname = "eta_{electron}";
+  g_Xname = "eta_{mu}";
   g_Xmin = 0.0;
 
 
@@ -206,7 +206,7 @@ void Plot_1D_stack(){
 	if(base->Nlep != 1)
     continue;
 
-  if(base->PDGID_lep->at(0) != 11 || base->PDGID_lep->at(0) != -11)
+  if(base->PDGID_lep->at(0) != 13 && base->PDGID_lep->at(0) != -13)
     continue;
 
   // if(base->Nlep != 3) //medium working point
