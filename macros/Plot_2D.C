f6df8faa383f93189460b5c0ec2d69b1e16fbfbf
Margaret Lazarovits <margaretlazarovits@Margarets-Air-2.attlocal.net>
2019-07-09 11:44:55 -0500

    risr vs ptlep signal 200-1 gev

--- macros/Plot_2D.C
+++ macros/Plot_2D.C
@@ -138,7 +138,7 @@ void Plot_2D(){
 
   int Nsample = g_Samples.size();
   //string g_Label = "No selection";
-  string g_Label = "pT_ISR vs. pT_lep, Cat. 3 (sig: 200-1 GeV)";
+  string g_Label = "RISR vs. pT_lep, Cat. 3 (sig: 200-1 GeV)";
 
 
   g_Yname = "pT_lep (GeV)";
@@ -147,10 +147,10 @@ void Plot_2D(){
   units_per_bin_y = 1.;
   g_NY = (int)(g_Ymax - g_Ymin)/units_per_bin_y;
 
-  g_Xname = "pT_ISR (GeV)";
+  g_Xname = "RISR";
   g_Xmin = 0.0;
-  g_Xmax = 1000.; 
-  units_per_bin_x = 10.;
+  g_Xmax = 1.3.; 
+  units_per_bin_x = 0.1;
   g_NX = (int)(g_Xmax - g_Xmin)/units_per_bin_x;
 
   // g_Xname = "Njets_S";
@@ -203,7 +203,7 @@ void Plot_2D(){
   if(base->Nlep != 1)
     continue;
       
-	hist->Fill(base->PTISR->at(2), base->PT_lep->at(0), base->weight*double(SKIP));
+	hist->Fill(base->RISR->at(2), base->PT_lep->at(0), base->weight*double(SKIP));
       }
 
       delete base;
