8585d333246d54990414a3a108512c32c32ca418
Margaret Lazarovits <margaretlazarovits@Margarets-Air-2.attlocal.net>
2019-07-19 10:32:20 -0500

    reducedbasenew test

--- include/ReducedBase_new.hh
+++ include/ReducedBase_new.hh
@@ -313,7 +313,6 @@ public :
    virtual Int_t    GetEntry(Long64_t entry);
    virtual Long64_t LoadTree(Long64_t entry);
    virtual void     Init(TTree *tree);
-   virtual void     Loop();
    virtual Bool_t   Notify();
    virtual void     Show(Long64_t entry = -1);
 };
