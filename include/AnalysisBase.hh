c0c0acb71b174b9d6a3c392027b86cfb29c56f0b
Christopher Rogan <christopherrogan@Christophers-MacBook-Pro.local>
2019-09-23 23:47:14 -0500

    added event count bookkeeping to ReducedNtuple

--- include/AnalysisBase.hh
+++ include/AnalysisBase.hh
@@ -47,18 +47,24 @@ public:
   double DeltaPhiMin(const vector<TLorentzVector>& JETs, const TVector3& MET, int N = -1);
   double DeltaPhiMin(const vector<pair<TLorentzVector, bool> >& JETs, const TVector3& MET, int N = -1);
   
-  void MomTensorCalc(vector<TLorentzVector>& input, vector<double>& eigenvalues, double pow = 1., bool threeD = true); 
+  void MomTensorCalc(vector<TLorentzVector>& input, vector<double>& eigenvalues, double pow = 1., bool threeD = true);
 
+  virtual std::pair<int,int> GetSUSYMasses();
+
+  bool IsSMS(){ return m_DoSMS; }
+  string GetDataSet(){ return m_DataSet; }
+  string GetFileTag(){ return m_FileTag; }
+  
 protected:
   bool m_DoSMS;
   
   virtual double GetEventWeight();
   virtual double GetXsec();
 
-
-private:
   string m_DataSet;
   string m_FileTag;
+  
+private:
 
   NeventTool m_NeventTool;
   XsecTool   m_XsecTool;
