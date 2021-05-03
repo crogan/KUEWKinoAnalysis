dd252ff7a1fc3cea93877da2b2643ad07e27252f
Christopher Rogan <christopherrogan@ChristophersMBP.attlocal.net>
2021-05-03 15:33:05 -0500

    additional changes to CMS Connect scripts

--- execute_script.sh
+++ execute_script.sh
@@ -6,10 +6,10 @@ tar -xzf ./config.tgz
 export SCRAM_ARCH=slc7_amd64_gcc700
 source /cvmfs/cms.cern.ch/cmsset_default.sh
 
-source ./config/cmssw_setup.sh
+source ./config/cmssw_setup_connect.sh
 
 cmssw_setup sandbox-CMSSW_10_6_5-6403d6f.tar.bz2
 
-source ./config/setup_RestFrames.sh 
+source ./config/setup_RestFrames_connect.sh 
 
 ./config/MakeReducedNtuple_NANO.x "$@"
