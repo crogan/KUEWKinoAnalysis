02f553f2924d2f682471bab5a381cfaab8f990d7
Christopher Rogan <christopherrogan@ChristophersMBP.attlocal.net>
2021-05-03 15:24:44 -0500

    added files for CMS Connect submission

---
+++ execute_script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+wget --no-check-certificate http://stash.osgconnect.net/+zflowers/sandbox-CMSSW_10_6_5-6403d6f.tar.bz2
+
+tar -xzf ./config.tgz
+
+export SCRAM_ARCH=slc7_amd64_gcc700
+source /cvmfs/cms.cern.ch/cmsset_default.sh
+
+source ./config/cmssw_setup.sh
+
+cmssw_setup sandbox-CMSSW_10_6_5-6403d6f.tar.bz2
+
+source ./config/setup_RestFrames.sh 
+
+./config/MakeReducedNtuple_NANO.x "$@"
