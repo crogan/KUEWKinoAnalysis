317ffeba2f74f350d4ca74b1186fa8dea6ab07a9
Margaret Lazarovits <mrlazarovits@gmail.com>
2021-05-05 13:34:36 -0500

    looking at 3L categories

--- macros/TestReader.C
+++ macros/TestReader.C
@@ -18,20 +18,26 @@ FitReader* FITReader = new FitReader(inputfile, a2, a3);
   CategoryTree CT_3L = CTTool.GetCategories_3L();
   CategoryTree CT_Fakes3L = CTTool.GetCategories_Fakes3L();
 //FITReader->PrintCategories();
-//CT_Fakes1L.Print(); 
-//vector<const CategoryTree*> catTrees;
-//CT_Fakes1L.GetListDepth(catTrees,1);
-//for(int i = 0; i < int(catTrees.size()); i++){
-//cout << "list #" << i << endl;
-//catTrees[i]->Print();
-//}
+//FITReader->PrintProcesses();
+  CategoryTree CT_test = CT_Fakes3L;
+CT_test.Print(); 
+vector<const CategoryTree*> catTrees;
+CT_test.GetListDepth(catTrees,1);
+CategoryList catList = FITReader->GetCategories();
+for(int i = 0; i < int(catTrees.size()); i++){
+cout << "list #" << i << " " << catTrees[i]->GetSpecLabel() << endl;
+CategoryList cats = catList.Filter(*catTrees[i]);
+cats.Print();
+}
 //  FITReader->PlotYields("0Lyields",
 //			VS().a("ttbar").a("ST").a("DB").a("ZDY").a("TB").a("QCD")
 //			.a("Wjets").a("Fakes").a("T2tt_5000490").a("T2tt_5000480").a("T2tt_5000375"),
 //			CT_0L);
 //  FITReader->PlotYields("1Lyields",
-//			VS().a("ttbar").a("ST").a("DB").a("ZDY").a("TB").a("QCD")
-//			.a("Wjets").a("Fakes").a("T2tt_5000490").a("T2tt_5000480").a("T2tt_5000375"),
+//			VS().a("QCD").a("ttbar").a("ST").a("DB").a("ZDY").a("TB").a("Wjets").a("Fakes"),
+//			CT_1L);
+//  FITReader->PlotYields("1Lyields_HF",
+//			VS().a("ttbar_HF").a("ST_HF").a("DB_HF").a("ZDY_HF").a("TB_HF").a("Wjets_HF").a("HF"),
 //			CT_1L);
 //  FITReader->PlotYields("2Lyields",
 //			VS().a("ttbar").a("ST").a("DB").a("ZDY").a("TB").a("QCD")
@@ -42,8 +48,8 @@ FitReader* FITReader = new FitReader(inputfile, a2, a3);
 //			.a("Wjets").a("Fakes").a("T2tt_5000490").a("T2tt_5000480").a("T2tt_5000375"),
 //			CT_3L);
 
-  // FITReader->Plot1Dstack(SL().a("ttbar").a("ST").a("DB").a("ZDY").a("TB")
-  // 			 .a("Wjets").a("Fakes").a("T2bW_50000490").a("T2bW_50000480").a("T2bW_50000470"),
+//   FITReader->Plot1Dstack(VS().a("ttbar").a("ST").a("DB").a("ZDY").a("TB")
+//   			 .a("Wjets").a("Fakes").a("T2bW_50000490").a("T2bW_50000480").a("T2bW_50000470"),
   // FITReader->Plot1Dstack(VS().a("ttbar").a("ST").a("DB").a("ZDY").a("TB")
   // 			 .a("Wjets").a("Fake").a("HF").a("LF").a("T2tt_5000470").a("T2tt_5000450").a("T2tt_5000420"),
   // 			 VS().a("1L"),
@@ -69,36 +75,38 @@ FitReader* FITReader = new FitReader(inputfile, a2, a3);
   // 			 "canvas3");
 //the args with multiple entries are the ones that go into the total for the ratio
 const string& proc = "ttbar_HF"; 
-const string& source = "1";
+const string& source = "LF";
+const string& numSource = "0";
 const string& lep = "el";
-const string& hadS = "1jS";
+const string& hadS = "0jS";
 const string& hadI = "ge1jISR";
-const string& lep1 = "1Lgold";
-const string& lep2 = "1Lsilver";
-const string& lep3 = "1Lbronze";
-const string& lep4 = "3Lmumumu";
-const string& hadS1 = "1jS";
-const string& hadS2 = "2jS";
-const string& hadS3 = "3jS";
- //   TCanvas* cv1 = FITReader->Plot1Dstraight(proc,
- //       		 VS().a(lep1).a(lep2).a(lep3),
- //       		 VS().a(hadS),
- //       		 VS().a(hadI),
- // 			 "canvas2",
- //       		 VS());
-    TCanvas* cv = FITReader->Plot1Dratio(proc,
-        		 VS().a(lep1).a(lep2).a(lep3),
-        		 VS().a(hadS),
- 	     		 VS().a(hadI),
-  			 "canvas3",
- 			 VS());
-//    TCanvas* cv = FITReader->Plot1DratioProc(VS().a("ttbar_Fakes_"+lep+"f"+source).a("Wjets_Fakes_"+lep+"f"+source).a("ZDY_Fakes_"+lep+"f"+source).a("ST_Fakes_"+lep+"f"+source).a("TB_Fakes_"+lep+"f"+source),
-//        		 "1L",
-//        		 hadS,		
-// 	     		 hadI,
+
+const string& lep1 = "3Lelgold";
+const string& lep2 = "3Lelsilver";
+const string& lep3 = "3Lelbronze";
+const string& lep4 = "3Lelbronze";
+const string& hadS1 = "2j0bS";
+const string& hadS2 = "2j1bS";
+const string& hadS3 = "2j2bS";
+  //  TCanvas* cv1 = FITReader->Plot1Dstraight(proc,
+  //      		 VS().a(lep1).a(lep2).a(lep3),
+  //      		 VS().a(hadS),
+  //      		 VS().a(hadI),
+  //      		 "canvas2",
+  //      		 VS());
+//    TCanvas* cv = FITReader->Plot1Dratio(proc,
+//        		 VS().a(lep1).a(lep2).a(lep3),
+//        		 VS().a(hadS),
+// 	     		 VS().a(hadI),
 //  			 "canvas3",
-// 			 "");
-  
+// 			 VS());
+  //  TCanvas* cv = FITReader->Plot1DratioProc(VS().a("DB_Fakes_"+lep+"f"+numSource).a("ttbar_Fakes_"+lep+"f"+numSource),
+  //      		 "1L",
+  //      		 hadS,		
+  //           		 hadI,
+  //			 "canvas3",
+  //      		 "");
+  //
 // TCanvas* cv = FITReader->Plot2D(VS().a("T1bbbb_13001100"),
 //  			 VS().a("1L"),
 //        		 //VS().a("3j0bS").a("3j1bS").a("3jge2bS"),
