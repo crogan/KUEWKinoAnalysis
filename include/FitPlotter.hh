50a61a135bd47a98d6e7527ecd9fea89d1d43386
Margaret Lazarovits <mrlazarovits@gmail.com>
2021-09-23 12:01:50 -0500

    update FitPlotter

--- include/FitPlotter.hh
+++ include/FitPlotter.hh
@@ -32,7 +32,8 @@ public:
 		       const VS& lep_cat,
 		       const VS& hadS_cat,
 		       const VS& hadI_cat,
-		       const string& canvas);
+		       const string& name,
+		       const VS& extra = VS());
 
   TCanvas* Plot2D(const VS& proc,
 		  const VS& lep_cat,
