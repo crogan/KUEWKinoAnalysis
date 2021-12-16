b60f530d4cd01e6882252b6c5d7538a8691d0020
mlazarovits <mrlazarovits@gmail.com>
2021-12-16 16:03:52 -0600

    filling luminum with m_luminum instead of m_runnum

--- src/ReducedNtuple.cc
+++ src/ReducedNtuple.cc
@@ -216,7 +216,7 @@ TTree* ReducedNtuple<Base>::InitOutputTree(const string& sample){
   tree->Branch("MetTrigSFweight_down", &m_MetTrigSFweight_down);
 
   tree->Branch("runnum", &m_runnum);
-  tree->Branch("luminum", &m_runnum);
+  tree->Branch("luminum", &m_luminum);
   tree->Branch("eventnum", &m_eventnum);
 
   tree->Branch("NPV", &m_NPV);
