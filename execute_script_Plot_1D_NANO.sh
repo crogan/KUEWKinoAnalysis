bd77c5e1766fafde63f792b78ccadb5e05685270
Zachary <zacharyflowers96@gmail.com>
2022-01-05 11:40:33 -0600

    Add WJets HT Code

---
+++ execute_script_Plot_1D_NANO.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#wget --quiet --no-check-certificate http://stash.osgconnect.net/+zflowers/sandbox-CMSSW_10_6_5-6403d6f.tar.bz2
+
+
+export SCRAM_ARCH=slc7_amd64_gcc700
+source /cvmfs/cms.cern.ch/cmsset_default.sh
+
+source cmssw_setup_connect.sh
+
+cmssw_setup sandbox-CMSSW_10_6_5-6403d6f.tar.bz2
+
+source setup_RestFrames_connect.sh 
+root.exe -l -b "Condor_Plot_1D_NANO.C++($@)"
+#rm _condor_stdout
