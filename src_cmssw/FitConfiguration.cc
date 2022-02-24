2569039ed4c9663893402cd7be02ebaec1c1934c
mlazarovits <mrlazarovits@gmail.com>
2022-02-23 19:43:46 -0600

    update FitConfig

--- src_cmssw/FitConfiguration.cc
+++ src_cmssw/FitConfiguration.cc
@@ -51,16 +51,16 @@ else{
   bkg_rare += "ST";
   bkg_rare += "TB";
 //float scale_DB in 2017
-if(*cb.era_set().find("2017") != *cb.era_set().end()) cout << "era: 2017" << endl;
+//if(*cb.era_set().find("2017") != *cb.era_set().end()) cout << "era: 2017" << endl;
 if(*cb.era_set().begin() == "2017") bkg_rate += "DB";
 else bkg_rare += "DB";
-  bkg_boson += "Wjets";
-  bkg_boson += "ZDY";
-  bkg_boson += "TB";
-  bkg_boson += "DB";
+ // bkg_boson += "Wjets";
+ // bkg_boson += "ZDY";
+ // bkg_boson += "TB";
+ // bkg_boson += "DB";
 
-  bkg_top += "ttbar";
-  bkg_top += "ST";
+ // bkg_top += "ttbar";
+ // bkg_top += "ST";
 
 
   // allow for regex filtering
@@ -86,7 +86,7 @@ else bkg_rare += "DB";
    //   	cb.cp().bin(VS().a(".*0L.*").a(".*1L.*")).process(plist.GetProcesses())
    //       .AddSyst(cb,"scale_"+p,"lnN",SystMap<>::init(1.2));
    // }
-    else 
+    //else 
     cb.cp().process(plist.GetProcesses())
        .AddSyst(cb, "scale_"+p, "lnN", SystMap<>::init(1.2));
   }
