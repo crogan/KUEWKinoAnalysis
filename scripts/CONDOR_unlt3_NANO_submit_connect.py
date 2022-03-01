0b2425684a8409293a01dc6b94c675c4bee64cb0
Zachary Flowers <zacharyflowers96@gmail.com>
2022-03-01 11:53:52 -0600

    fix json bug

--- scripts/CONDOR_unlt3_NANO_submit_connect.py
+++ scripts/CONDOR_unlt3_NANO_submit_connect.py
@@ -211,6 +211,7 @@ if __name__ == "__main__":
 
     # make json file
     os.system("cat json/GoodRunList/*.txt > "+config+"GRL_JSON.txt")
+    os.system("echo -n $(tr -d '\n' < "+config+"GRL_JSON.txt) > "+config+"GRL_JSON.txt")
     JSON = "./config/GRL_JSON.txt"
 
     # copy PU root files
