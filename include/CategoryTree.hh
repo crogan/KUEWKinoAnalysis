7574eb37af863fa5037e5a71b560077fe2b58046
mlazarovits <mrlazarovits@gmail.com>
2022-07-19 20:59:48 -0500

    starting work on PTISR/gamT plots

--- include/CategoryTree.hh
+++ include/CategoryTree.hh
@@ -132,7 +132,9 @@ public:
   CategoryTree GetCategories_Wjets3L() const;
   CategoryTree GetCategories_1L_ChargeSep() const;
   CategoryTree GetCategories_0L_plotFormat() const;
+  CategoryTree GetCategories_0L_fineSplit() const;
   CategoryTree GetCategories_1L_plotFormat() const;
+  CategoryTree GetCategories_1L_fineSplit() const;
   CategoryTree GetCategories_2L_plotFormat() const;
   CategoryTree GetCategories_3L_plotFormat() const;
   CategoryTree GetCategories_Plot1L() const;
