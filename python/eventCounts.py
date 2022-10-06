819c312a35e677c8b0b17623ba4171dd8df21e86
Caleb James Smith <colorado.caleb.smith@gmail.com>
2022-10-06 12:22:43 -0500

    Get number of events saved in ntuples.

--- python/eventCounts.py
+++ python/eventCounts.py
@@ -10,12 +10,18 @@ ROOT.gROOT.SetBatch(ROOT.kTRUE)
 # Tell ROOT not to be in charge of memory, fix issue of histograms being deleted when ROOT file is closed:
 ROOT.TH1.AddDirectory(False)
 
-def getProcessedEvents(root_file):
+# number of processed events (run over when creating ntuples, before cuts)
+def getProcessedEvents(root_file, tree_name, is_signal):
     nevents = -1
     return nevents
 
-def getNtupleEvents(root_file):
+# number of ntuple events (saved in ntuples after cuts)
+def getNtupleEvents(root_file, tree_name, is_signal):
     nevents = -1
+    chain   = ROOT.TChain(tree_name)
+    chain.Add(root_file)
+    nevents = chain.GetEntries()
+    nevents = int(nevents)
     return nevents
 
 def createCSV(output_csv, samples):
@@ -24,11 +30,12 @@ def createCSV(output_csv, samples):
         csv_writer = csv.writer(f)
         csv_writer.writerow(column_titles)
         for sample in samples:
-            is_signal       = samples[sample]["is_signal"] 
             path            = samples[sample]["path"] 
+            tree_name       = samples[sample]["tree_name"] 
+            is_signal       = samples[sample]["is_signal"] 
             
-            nevents_processed   = getProcessedEvents(path)
-            nevents_ntuple      = getNtupleEvents(path)
+            nevents_processed   = getProcessedEvents(path, tree_name, is_signal)
+            nevents_ntuple      = getNtupleEvents(path, tree_name, is_signal)
             
             row = [sample, nevents_processed, nevents_ntuple]
             csv_writer.writerow(row)
@@ -44,6 +51,8 @@ def eventCounts():
     samples["T2_4bd_500_490"]   = {}
     samples["TTJets_DiLept"]["is_signal"]   = False
     samples["T2_4bd_500_490"]["is_signal"]  = True
+    samples["TTJets_DiLept"]["tree_name"]   = "KUAnalysis"
+    samples["T2_4bd_500_490"]["tree_name"]  = "SMS_500_490"
     samples["TTJets_DiLept"]["path"]        = "root://cmseos.fnal.gov//store/user/lpcsusylep/NTUPLES_NanoAODv9/TTjets_DILEP.root"
     samples["T2_4bd_500_490"]["path"]       = "root://cmseos.fnal.gov//store/user/lpcsusylep/NTUPLES_NanoAODv9/SMS-T2-4bd_genMET-80_mStop-500_mLSP-490_TuneCP5_13TeV-madgraphMLM-pythia8_UL2017_NanoAODv9_.root"
     createCSV(output_csv, samples)
