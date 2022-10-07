86902c9d5cfd941e0fb24398cde172e8f9f380c2
Caleb James Smith <colorado.caleb.smith@gmail.com>
2022-10-07 16:49:42 -0500

    Use sample list to define order of samples.

--- python/getWeights.py
+++ python/getWeights.py
@@ -5,10 +5,10 @@ import csv
 import tools
 
 # TODO
-# - Add Wjets and Zjets samples
 
 # DONE
 # - Get event weights from PreUL samples
+# - Add Wjets and Zjets samples
 
 # Make sure ROOT.TFile.Open(fileURL) does not seg fault when $ is in sys.argv (e.g. $ passed in as argument)
 ROOT.PyConfig.IgnoreCommandLineOptions = True
@@ -26,7 +26,7 @@ def getWeight(root_file, tree_name, is_signal):
     return weight
 
 # create csv with number of events
-def createCSV(output_csv, samples, lumi):
+def createCSV(output_csv, sample_list, samples, lumi):
     print("Creating {0}".format(output_csv))
     
     column_titles = ["sample", "lumi", "weight_ntuple", "weight_lumi"]
@@ -34,17 +34,14 @@ def createCSV(output_csv, samples, lumi):
     with open(output_csv, 'w') as f:
         csv_writer = csv.writer(f)
         csv_writer.writerow(column_titles)
-        for sample in samples:
+        # use sample list to determine order
+        for sample in sample_list:
             path            = samples[sample]["path"] 
             tree_name       = samples[sample]["tree_name"] 
             is_signal       = samples[sample]["is_signal"] 
 
             weight_ntuple   = getWeight(path, tree_name, is_signal)
             weight_lumi     = lumi * weight_ntuple
-            
-            #nevents_processed   = getProcessedEvents(path, "EventCount", is_signal)
-            #nevents_ntuple      = getNtupleEvents(path, tree_name, is_signal)
-            #acceptance          = float(nevents_ntuple) / float(nevents_processed)
             
             row = [sample, lumi, weight_ntuple, weight_lumi]
             csv_writer.writerow(row)
@@ -61,6 +58,7 @@ def getWeights():
     if ntuple_path[-1] != "/":
         ntuple_path += "/"
 
+    # Sample lists
     signal_samples = [
         "T2_4bd_500_490",
     ]
@@ -82,7 +80,9 @@ def getWeights():
         "WJetsToLNu_HT-1200To2500",
         "WJetsToLNu_HT-2500ToInf",
     ]
+    all_samples = signal_samples + background_samples
 
+    # Sample map
     samples = {}
     for sample in signal_samples:
         samples[sample] = {}
@@ -92,14 +92,6 @@ def getWeights():
         samples[sample] = {}
         samples[sample]["is_signal"] = False
         samples[sample]["tree_name"] = "KUAnalysis"
-    
-    #samples = {}
-    #samples["T2_4bd_500_490"]   = {}
-    #samples["TTJets_DiLept"]    = {}
-    #samples["T2_4bd_500_490"]["is_signal"]  = True
-    #samples["TTJets_DiLept"]["is_signal"]   = False
-    #samples["T2_4bd_500_490"]["tree_name"]  = "SMS_500_490"
-    #samples["TTJets_DiLept"]["tree_name"]   = "KUAnalysis"
 
     samples["T2_4bd_500_490"]["path"]               = ntuple_path + "Fall17_102X_SMS/SMS-T2-4bd_genMET-80_mStop-500_mLSP-490_TuneCP5_13TeV-madgraphMLM-pythia8_Fall17_102X.root"
     samples["TTJets_DiLept"]["path"]                = ntuple_path + "Fall17_102X/TTJets_DiLept_TuneCP5_13TeV-madgraphMLM-pythia8_Fall17_102X.root"
@@ -119,7 +111,7 @@ def getWeights():
     samples["WJetsToLNu_HT-1200To2500"]["path"]     = ntuple_path + "Fall17_102X/WJetsToLNu_HT-1200To2500_TuneCP5_13TeV-madgraphMLM-pythia8_Fall17_102X.root"
     samples["WJetsToLNu_HT-2500ToInf"]["path"]      = ntuple_path + "Fall17_102X/WJetsToLNu_HT-2500ToInf_TuneCP5_13TeV-madgraphMLM-pythia8_Fall17_102X.root"
 
-    createCSV(output_csv, samples, lumi)
+    createCSV(output_csv, all_samples, samples, lumi)
 
 def main():
     getWeights()
