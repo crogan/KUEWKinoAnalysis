dd56d81e56762a9aa81b3b284b4836719a5201b3
Justin Anguiano <justin.anguiano1@gmail.com>
2023-05-09 11:03:28 -0500

    samples have been run and saved to eos, renorm added in post

--- scripts/TChiWZ_Integral_Reweight/CSVReweight_tools/RunCSVWeights.sh
+++ scripts/TChiWZ_Integral_Reweight/CSVReweight_tools/RunCSVWeights.sh
@@ -1,2 +1,8 @@
-./addCSVWeights TChiWZ_genHT-160_genMET-80_TuneCP2_13TeV-madgraphMLM-pythia8_Summer16_102X.root
+#./addCSVWeights ../../../TChiWZ_genHT-160_genMET-80_TuneCP2_13TeV-madgraphMLM-pythia8_Summer16_102X.root
 
+
+#./addCSVWeights ../../../TChiWZ_genHT-160_genMET-80_TuneCP2_13TeV-madgraphMLM-pythia8_Autumn18_102X.root
+
+#./addCSVWeights ../../../TChiWZ_genHT-160_genMET-80_TuneCP2_13TeV-madgraphMLM-pythia8_Summer16_102X.root
+
+./addCSVWeights ../../../TChiWZ_genHT-160_genMET-80_TuneCP2_13TeV-madgraphMLM-pythia8_Fall17_102X.root
