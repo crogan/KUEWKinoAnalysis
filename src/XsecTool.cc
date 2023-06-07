e4133f95e9ef2a154397ea7c684593c5ea2945d9
Zachary <zacharyflowers96@gmail.com>
2023-06-07 13:19:30 -0500

    add TTWW list

--- src/XsecTool.cc
+++ src/XsecTool.cc
@@ -376,6 +376,8 @@ std::map<std::string,double> XsecTool::InitMap_Xsec_BKG(){
   Label2Xsec["TTWJetsToQQ_TuneCUETP8M1_13TeV-amcatnloFXFX-madspin-pythia8"] = 0.4062;
   Label2Xsec["TTZToLLNuNu_M-10_TuneCUETP8M1_13TeV-amcatnlo-pythia8"] = 0.2529;
   Label2Xsec["TTZToQQ_TuneCUETP8M1_13TeV-amcatnlo-pythia8"] = 0.5297;
+  Label2Xsec["TWW_TuneCUETP8M2T4_13TeV-madgraph-pythia8"] = 0.007834;
+  Label2Xsec["TTWW_TuneCP5_13TeV-madgraph-pythia8"] = 0.006992;
 
   Label2Xsec["TTWH_TuneCP5_13TeV-madgraph-pythia8"] = 0.001141;
   Label2Xsec["TTWW_TuneCP5_13TeV-madgraph-pythia8"] = 0.006979;
