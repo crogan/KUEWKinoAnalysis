1dd0f2ae0ac198c01352cc37ad1267c698fb8152
Justin Anguiano <justin.anguiano1@gmail.com>
2023-10-06 11:55:19 -0500

    corrected comments on sms flag

--- MakeNtuple_LPC/runGenSubmission.sh
+++ MakeNtuple_LPC/runGenSubmission.sh
@@ -1,7 +1,7 @@
 
 #add space delimited flags (--slim is already automatically included)
-#signal: --SMS  
-#fastsim signal: --SMS --fastsim
+#signal: --sms  
+#fastsim signal: --sms --fastsim
 #data: --data
 #MC: --sys
 python generateSubmission.py /uscms/home/janguian/nobackup/CMSSW_10_6_5/src/KUEWKinoAnalysis/samples/NANO/Summer16_102X/DYJetsToLL_M-50_HT-1200to2500_TuneCUETP8M1_13TeV-madgraphMLM-pythia8.txt --sys
