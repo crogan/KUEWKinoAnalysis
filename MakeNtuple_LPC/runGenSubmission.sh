038ff588acac06703f84174ce83a3a7009d673ba
Justin Anguiano <justin.anguiano1@gmail.com>
2023-10-06 12:00:04 -0500

    added --sys to signal comments

--- MakeNtuple_LPC/runGenSubmission.sh
+++ MakeNtuple_LPC/runGenSubmission.sh
@@ -1,7 +1,7 @@
 
 #add space delimited flags (--slim is already automatically included)
-#signal: --sms  
-#fastsim signal: --sms --fastsim
+#signal: --sms --sys 
+#fastsim signal: --sms --fastsim --sys
 #data: --data
 #MC: --sys
 python generateSubmission.py /uscms/home/janguian/nobackup/CMSSW_10_6_5/src/KUEWKinoAnalysis/samples/NANO/Summer16_102X/DYJetsToLL_M-50_HT-1200to2500_TuneCUETP8M1_13TeV-madgraphMLM-pythia8.txt --sys
