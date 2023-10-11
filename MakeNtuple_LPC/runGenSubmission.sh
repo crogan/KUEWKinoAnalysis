da21be86e7aa407f367844a5e5aa52851b08015e
Caleb James Smith <colorado.caleb.smith@gmail.com>
2023-10-11 14:30:02 -0500

    Update header comments in runGenSubmission.sh

--- MakeNtuple_LPC/runGenSubmission.sh
+++ MakeNtuple_LPC/runGenSubmission.sh
@@ -1,10 +1,12 @@
-
-#add space delimited flags (--slim is already automatically included)
-#signal: --sms --sys 
-#fastsim signal: --sms --fastsim --sys
-#data: --data
-#MC: --sys
-
+# ------------------------------------------- #
+# add space delimited flags (--slim is already automatically included)
+# - signal: --sms --sys 
+# - fastsim signal: --sms --fastsim --sys
+# - data: --data
+# - MC: --sys
+# - all samples: -filetag=[Summer16_102X,Fall17_102X,Autumn18_102X], according to year [2016,2017,2018]
+# - job splitting: use -split=N, where N is the number of jobs per input file
+# ------------------------------------------- #
 
 ##Launched
 python generateSubmission.py /uscms/home/janguian/nobackup/CMSSW_10_6_5/src/KUEWKinoAnalysis/samples/NANO/Summer16_102X/DYJetsToLL_M-50_HT-1200to2500_TuneCUETP8M1_13TeV-madgraphMLM-pythia8.txt --sys -filetag=Summer16_102X -split=20
