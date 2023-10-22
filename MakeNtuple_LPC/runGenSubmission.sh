41076b3b8e80f4d981183dbab752c7835ebd2eeb
Justin Anguiano <justin.anguiano1@gmail.com>
2023-10-22 16:45:40 -0500

    fixing merge issues with script

--- MakeNtuple_LPC/runGenSubmission.sh
+++ MakeNtuple_LPC/runGenSubmission.sh
@@ -6,18 +6,31 @@
 #MC: --sys
 
 
-##Launched
-python generateSubmission.py /uscms/home/janguian/nobackup/CMSSW_10_6_5/src/KUEWKinoAnalysis/samples/NANO/Summer16_102X/DYJetsToLL_M-50_HT-1200to2500_TuneCUETP8M1_13TeV-madgraphMLM-pythia8.txt --sys -filetag=Summer16_102X -split=20
-
+##TEST Launches
+#python generateSubmission.py /uscms/home/janguian/nobackup/CMSSW_10_6_5/src/KUEWKinoAnalysis/samples/NANO/Summer16_102X/DYJetsToLL_M-50_HT-1200to2500_TuneCUETP8M1_13TeV-madgraphMLM-pythia8.txt --sys -filetag=Summer16_102X -split=20
 #python generateSubmission.py /uscms/home/janguian/nobackup/CMSSW_10_6_5/src/KUEWKinoAnalysis/samples/NANO/Fall17_102X/DYJetsToLL_M-4to50_HT-100to200_TuneCP5_13TeV-madgraphMLM-pythia8.txt --sys -filetag=Fall17_102X
 
 
-python generateSubmission.py /uscms/home/janguian/nobackup/CMSSW_10_6_5/src/KUEWKinoAnalysis/samples/NANO/Summer16_102X/DYJetsToLL_M-50_HT-200to400_TuneCUETP8M1_13TeV-madgraphMLM-pythia8.txt --sys -filetag=Summer16_102X -split=2
-#python generateSubmission.py /uscms/home/janguian/nobackup/CMSSW_10_6_5/src/KUEWKinoAnalysis/samples/NANO/Summer16_102X/DYJetsToLL_M-50_HT-2500toInf_TuneCUETP8M1_13TeV-madgraphMLM-pythia8.txt --sys
+#Running
+#python generateSubmission.py /uscms/home/janguian/nobackup/CMSSW_10_6_5/src/KUEWKinoAnalysis/samples/NANO/Summer16_102X/DYJetsToLL_M-50_HT-100to200_TuneCUETP8M1_13TeV-madgraphMLM-pythia8.txt --sys -filetag=Summer16_102X -split=30
+#python generateSubmission.py /uscms/home/janguian/nobackup/CMSSW_10_6_5/src/KUEWKinoAnalysis/samples/NANO/Summer16_102X/DYJetsToLL_M-50_HT-200to400_TuneCUETP8M1_13TeV-madgraphMLM-pythia8.txt --sys -filetag=Summer16_102X -split=10
+#python generateSubmission.py /uscms/home/janguian/nobackup/CMSSW_10_6_5/src/KUEWKinoAnalysis/samples/NANO/Summer16_102X/DYJetsToLL_M-50_HT-2500toInf_TuneCUETP8M1_13TeV-madgraphMLM-pythia8.txt --sys -filetag=Summer16_102X -split=10
+#python generateSubmission.py /uscms/home/janguian/nobackup/CMSSW_10_6_5/src/KUEWKinoAnalysis/samples/NANO/Summer16_102X/DYJetsToLL_M-50_HT-400to600_TuneCUETP8M1_13TeV-madgraphMLM-pythia8.txt --sys -filetag=Summer16_102X -split=10
+#python generateSubmission.py /uscms/home/janguian/nobackup/CMSSW_10_6_5/src/KUEWKinoAnalysis/samples/NANO/Summer16_102X/DYJetsToLL_M-50_HT-600to800_TuneCUETP8M1_13TeV-madgraphMLM-pythia8.txt --sys -filetag=Summer16_102X -split=10
+#python generateSubmission.py /uscms/home/janguian/nobackup/CMSSW_10_6_5/src/KUEWKinoAnalysis/samples/NANO/Summer16_102X/DYJetsToLL_M-50_HT-70to100_TuneCUETP8M1_13TeV-madgraphMLM-pythia8.txt --sys -filetag=Summer16_102X -split=10
+#python generateSubmission.py /uscms/home/janguian/nobackup/CMSSW_10_6_5/src/KUEWKinoAnalysis/samples/NANO/Summer16_102X/DYJetsToLL_M-50_HT-800to1200_TuneCUETP8M1_13TeV-madgraphMLM-pythia8.txt --sys -filetag=Summer16_102X -split=10
+#python generateSubmission.py /uscms/home/janguian/nobackup/CMSSW_10_6_5/src/KUEWKinoAnalysis/samples/NANO/Summer16_102X/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8.txt --sys -filetag=Summer16_102X -split=10
+#python generateSubmission.py /uscms/home/janguian/nobackup/CMSSW_10_6_5/src/KUEWKinoAnalysis/samples/NANO/Summer16_102X/DYJetsToLL_M-5to50_HT-100to200_TuneCUETP8M1_13TeV-madgraphMLM-pythia8.txt --sys -filetag=Summer16_102X -split=30
+#python generateSubmission.py /uscms/home/janguian/nobackup/CMSSW_10_6_5/src/KUEWKinoAnalysis/samples/NANO/Summer16_102X/DYJetsToLL_M-5to50_HT-200to400_TuneCUETP8M1_13TeV-madgraphMLM-pythia8.txt --sys -filetag=Summer16_102X -split=30
+#python generateSubmission.py /uscms/home/janguian/nobackup/CMSSW_10_6_5/src/KUEWKinoAnalysis/samples/NANO/Summer16_102X/DYJetsToLL_M-5to50_HT-400to600_TuneCUETP8M1_13TeV-madgraphMLM-pythia8.txt --sys -filetag=Summer16_102X -split=30
+#python generateSubmission.py /uscms/home/janguian/nobackup/CMSSW_10_6_5/src/KUEWKinoAnalysis/samples/NANO/Summer16_102X/DYJetsToLL_M-5to50_HT-600toInf_TuneCUETP8M1_13TeV-madgraphMLM-pythia8.txt --sys -filetag=Summer16_102X -split=30
+#python generateSubmission.py /uscms/home/janguian/nobackup/CMSSW_10_6_5/src/KUEWKinoAnalysis/samples/NANO/Summer16_102X/DYJetsToLL_M-5to50_HT-70to100_TuneCUETP8M1_13TeV-madgraphMLM-pythia8.txt --sys -filetag=Summer16_102X -split=30
 
-## NotLaunched
-#python generateSubmission.py /uscms/home/janguian/nobackup/CMSSW_10_6_5/src/KUEWKinoAnalysis/samples/NANO/Summer16_102X/DYJetsToLL_M-50_HT-400to600_TuneCUETP8M1_13TeV-madgraphMLM-pythia8.txt --sys
-#python generateSubmission.py /uscms/home/janguian/nobackup/CMSSW_10_6_5/src/KUEWKinoAnalysis/samples/NANO/Summer16_102X/DYJetsToLL_M-50_HT-600to800_TuneCUETP8M1_13TeV-madgraphMLM-pythia8.txt --sys
-#python generateSubmission.py /uscms/home/janguian/nobackup/CMSSW_10_6_5/src/KUEWKinoAnalysis/samples/NANO/Summer16_102X/DYJetsToLL_M-50_HT-70to100_TuneCUETP8M1_13TeV-madgraphMLM-pythia8.txt --sys
-#python generateSubmission.py /uscms/home/janguian/nobackup/CMSSW_10_6_5/src/KUEWKinoAnalysis/samples/NANO/Summer16_102X/DYJetsToLL_M-50_HT-800to1200_TuneCUETP8M1_13TeV-madgraphMLM-pythia8.txt --sys
+#Reruns (completed)
+#python generateSubmission.py /uscms/home/janguian/nobackup/CMSSW_10_6_5/src/KUEWKinoAnalysis/samples/NANO/Summer16_102X/DYJetsToLL_M-50_HT-800to1200_TuneCUETP8M1_13TeV-madgraphMLM-pythia8.txt --sys -filetag=Summer16_102X -split=100
+#python generateSubmission.py /uscms/home/janguian/nobackup/CMSSW_10_6_5/src/KUEWKinoAnalysis/samples/NANO/Summer16_102X/DYJetsToLL_M-50_HT-600to800_TuneCUETP8M1_13TeV-madgraphMLM-pythia8.txt --sys -filetag=Summer16_102X -split=100
+#python generateSubmission.py /uscms/home/janguian/nobackup/CMSSW_10_6_5/src/KUEWKinoAnalysis/samples/NANO/Summer16_102X/DYJetsToLL_M-50_HT-200to400_TuneCUETP8M1_13TeV-madgraphMLM-pythia8.txt --sys -filetag=Summer16_102X -split=100
 
+#reruns for problem files with big runtime (custom lists)
+#python generateSubmission.py /uscms/home/janguian/nobackup/CMSSW_10_6_5/src/KUEWKinoAnalysis/MakeNtuple_LPC/Rerun_Sample_Lists/DYJetsToLL_M-50_HT-100to200_TuneCUETP8M1_13TeV-madgraphMLM-pythia8-RERUN_Summer16_102X.txt --sys -filetag=Summer16_102X -split=300
+#python generateSubmission.py /uscms/home/janguian/nobackup/CMSSW_10_6_5/src/KUEWKinoAnalysis/MakeNtuple_LPC/Rerun_Sample_Lists/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8-RERUN_Summer16_102X.txt --sys -filetag=Summer16_102X -split=300
